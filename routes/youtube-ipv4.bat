377f9683afb07879afca4cf663612e164ebc376c
shlima <2356771+shlima@users.noreply.github.com>
2026-09-08 08:24:05 +0000

    Apply automatic changes

--- routes/youtube-ipv4.bat
+++ routes/youtube-ipv4.bat
@@ -500,6 +500,7 @@ route ADD 192.178.234.0 MASK 255.255.255.0 0.0.0.0 :: rem youtube
 route ADD 192.178.237.0 MASK 255.255.255.0 0.0.0.0 :: rem youtube
 route ADD 192.179.16.0 MASK 255.255.255.0 0.0.0.0 :: rem youtube
 route ADD 192.179.18.0 MASK 255.255.255.0 0.0.0.0 :: rem youtube
+route ADD 192.179.24.0 MASK 255.255.255.0 0.0.0.0 :: rem youtube
 route ADD 192.179.26.0 MASK 255.255.255.0 0.0.0.0 :: rem youtube
 route ADD 193.109.164.0 MASK 255.255.255.0 0.0.0.0 :: rem youtube
 route ADD 194.78.0.0 MASK 255.255.255.0 0.0.0.0 :: rem youtube
